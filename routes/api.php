e87393d30dfbfbbf0d11635817fc2b448849d0cb
Muhammad Rhojani <rhojanioja1@gmail.com>
2025-09-22 21:11:48 -0400

    https://github.com/rhojani1-oja/laravel-testing.git

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MathController; 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-Route::get('/add', [MathController::class, 'add']);
+use App\Http\Controllers\ProductController;
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
