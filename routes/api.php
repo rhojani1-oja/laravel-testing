704ec1d79befb019bbbd9badd65eb4286db8e914
Muhammad Rhojani <rhojanioja1@gmail.com>
2025-09-22 21:44:25 -0400

    https://github.com/rhojani1-oja/laravel-testing.git

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\MathController;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/add', [MathController::class, 'add']);
